75a13e5f2db07d685e426dd6a19e700175238a32
TomRight <zxvc@rambler.ru>
2022-10-11 14:32:48 +0500

    Почистил код

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,23 +1,16 @@
 <template>
-<h1>Список дел</h1>
-<TodoList></TodoList>  
+  <h1>Список дел</h1>
+  <TodoList></TodoList>
 </template>
 <script>
-// import {useTodoStore} from '../stores/todo.js'
-import { useTodoStore } from "/src/stores/todo";
-import TodoList from './TodoList.vue';
+import TodoList from "./TodoList.vue";
 export default {
-    data() {
-        return {
-            imputMessage: "",
-        };
-    },
-    methods: {
-        addTodo() {
-            this.todoStore.addNewTodo({ id: "none", name: this.imputMessage, done: false });
-        }
-    },
-    components: { TodoList }
+  data() {
+    return {
+      imputMessage: "",
+    };
+  },
+  components: { TodoList },
 };
 </script>
 <style></style>
