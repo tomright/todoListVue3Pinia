2f94d1f7a47c385b7d8ced3ccc8cc62f1e355495
TomRight <zxvc@rambler.ru>
2022-10-11 15:27:38 +0500

    Добавил и подключил иконки

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
+
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import "element-plus/dist/index.css";
 import ru from "element-plus/es/locale/lang/ru";
 
 const pinia = createPinia();
+
 const app = createApp(App);
 app
   .use(router)
@@ -16,3 +19,6 @@ app
     locale: ru,
   })
   .mount("#app");
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
